f18e22f0fe71ab2a49ae29fceadc9a3476dd82ec
Alberto G <agurrion@hotmail.com>
2023-01-31 06:51:20 -0600

    Update ChangeCalculatorServiceUnitTest.cs
    Adding a test case that asserts that when CASH machine doesn't have proper denominations for a change due it doesn't alter its inventory and returns the customer payment.

--- CashMaster.POS.Services.ChangeCalculatorService.UnitTest/ChangeCalculatorServiceUnitTest.cs
+++ CashMaster.POS.Services.ChangeCalculatorService.UnitTest/ChangeCalculatorServiceUnitTest.cs
@@ -15,15 +15,16 @@ namespace CashMaster.POS.Services.ChangeCalculatorServices.UnitTest
     {
         private IChangeCalculatorConfiguration _config;
         private ChangeCalculatorService _service;
+        Dictionary<string, string> _initialData;
 
         [SetUp]
         public void SetUp()
         {
-            Dictionary<string, string> initialData = new Dictionary<string, string>
+            _initialData = new Dictionary<string, string>
             {
-                {"Denominations", "0.01=0,0.05=0,0.10=0,0.25=0,0.50=0,1.00=0,2.00=0,5.00=0,10.00=0"}
+                {"Denominations", "0.01=0,0.05=0,0.10=0,0.25=0,0.50=0,1.00=0,2.00=0,5.00=0,10.00=1,20.00=0"}
             };
-            var _inMemoryConfig = new ConfigurationBuilder().AddInMemoryCollection(initialData!).Build();
+            var _inMemoryConfig = new ConfigurationBuilder().AddInMemoryCollection(_initialData!).Build();
             _config = new ChangeCalculatorConfigurationService(_inMemoryConfig);
            
             _service = new ChangeCalculatorService(_config);
@@ -33,14 +34,14 @@ namespace CashMaster.POS.Services.ChangeCalculatorServices.UnitTest
         public void ChangeCalculatorService_WhenUserDepositExactImport_ShouldReturnEmptyChange()
         {
             decimal itemPrice = 5.5m;
-            Dictionary<decimal, int> customerPayment = new Dictionary<decimal, int>
-        {
+            Dictionary<decimal, int> customerPayment = new()
+            {
             { 5.00m, 1 },
             { 0.50m, 1 }
         };
 
             var result = _service.CalculateChange(itemPrice, customerPayment);
-            Assert.That(result.Count, Is.EqualTo(0));
+            Assert.That(result, Is.Empty);
             
         }
 
@@ -48,30 +49,40 @@ namespace CashMaster.POS.Services.ChangeCalculatorServices.UnitTest
         public void ChangeCalculatorService_WhenUserDepositLessThanCost_ShouldThrowsArgumentException()
         {
             decimal itemPrice = 5.5m;
-            Dictionary<decimal, int> customerPayment = new Dictionary<decimal, int>
-        {
+            Dictionary<decimal, int> customerPayment = new()
+            {
             { 5.00m, 1 },
             { 0.25m, 1 }
         };
 
             var ex = Assert.Throws<ArgumentException>(() => _service.CalculateChange(itemPrice, customerPayment));
             Assert.That(ex.Message, Is.EqualTo("The total amount provided by the customer is not enough to cover the price of the item."));
+            
         }
 
         [Test]
         public void ChangeCalculatorService_WhenDoestHaveProperDenominations_ShouldReturnsCustomerPayment()
         {
             decimal itemPrice = 1.00m;
-            Dictionary<decimal, int> customerPayment = new Dictionary<decimal, int>
-        {
-            { 5.00m, 1 },
-            { 0.50m, 1 }
-            
-        };
-
+            Dictionary<decimal, int> customerPayment = new() { { 5.00m, 1 }, { 0.50m, 1 } };
             var result = _service.CalculateChange(itemPrice, customerPayment);
-
             Assert.That(result, Is.EqualTo(customerPayment));
+        }
+
+        [Test]
+        public void ChangeCalculatorService_WhenDoestHaveProperDenominations_ShouldNotUpdateItsDenominations()
+        {
+            decimal itemPrice = 9.00m;
+            Dictionary<decimal, int> customerPayment = new() { { 20.00m, 1 } };
+            var result = _service.CalculateChange(itemPrice, customerPayment);
+            var expectedResult = string
+                                .Join(",", _initialData.Select(kvp => string.Format("{0:0.00}={1}", kvp.Key, kvp.Value)))
+                                .Replace("Denominations=", "");
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.EqualTo(customerPayment));
+                Assert.That(string.Join(",", _service.Denominations.ToDictionary(kvp => kvp.Key, kvp => kvp.Value).Select(kvp => string.Format("{0:0.00}={1}", kvp.Key, kvp.Value))), Is.EqualTo(expectedResult));
+            });
         }
 
     }
